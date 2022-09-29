322f0fb0b89d96c2e0a2ef2ab96cd43845595917
91920 <91920@LAPTOP-1O8KP2P5>
2022-09-29 17:46:14 +0530

    First commit

---
+++ src/calculator.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class calculator {
+
+	
+		public static void main(String[] args)
+	
+		{
+	        Scanner sc = new Scanner(System.in);
+	 
+	        
+	        System.out.print(
+	            "Enter the first and the Second number - ");
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	 
+	        
+	        System.out.print(
+	            "Choose and Enter the type of operation you want to perform (+, -, *, /, %) - ");
+	        char op = sc.next().charAt(0);
+	        solve(a, b, op);
+	    }
+	    public static int solve(int a, int b, char op)
+	    {
+	        int ans = 0;
+	        
+	        if (op == '+') {
+	            ans = a + b;
+	           
+	        }
+	        else if (op == '-') {
+	            ans = a - b;
+	            
+	        }
+	        else if (op == '*') {
+	            ans = a * b;
+	          
+	        }
+	        else if (op == '%') {
+	            ans = a % b;
+	            
+	        }
+	        else if (op == '/') {
+	            ans = a / b;
+	        }
+	 
+	        
+	        System.out.println("Your answer is - " + ans);
+	        return ans;
+	    }
+	}
