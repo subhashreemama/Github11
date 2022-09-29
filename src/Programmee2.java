57bd5b6acb2966518c158f5c8a8c11b1b9cd9f5c
91920 <91920@LAPTOP-1O8KP2P5>
2022-09-29 22:33:40 +0530

    1st Round

---
+++ src/Programmee2.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Programmee2 {
+
+	
+		public static void main(String[] args) {
+			int i = 0;
+			int j = 0;
+			int a; 
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the value :");
+			a = sc.nextInt();
+			
+			List<String> list = new ArrayList<String>();
+			
+			while (j < a) {
+				
+				// if the number is not divisible by 2.
+				
+				if (i % 2 != 0) {
+					j++;
+					list.add(String.valueOf(i));
+				}
+				
+				i++;
+			}
+			
+			String s = String.join(",", list);
+			System.out.println(s);
+		}
+
+	}
