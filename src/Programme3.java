703382748db7267910af7a9e37d06b2f5d654bf7
91920 <91920@LAPTOP-1O8KP2P5>
2022-09-29 20:27:57 +0530

    1stRound

---
+++ src/Programme3.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Programme3 {
+
+	public static void main(String[] args) {
+		int a;
+		int i = 0;
+		int j = 0;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value :");
+
+		a = sc.nextInt();
+
+		if (a % 2 == 0) {
+			a--;
+		}
+
+		List<String> list = new ArrayList<String>();
+
+		while (j < a) {
+			// if the number is not divisible by 2.
+			if (i % 2 != 0) {
+				j++;
+				list.add(String.valueOf(i));
+			}
+			i++;
+		}
+		String s = String.join(",", list);
+		System.out.println(s);
+	}
+}
---
+++ src/Programme4.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class Programme4 {
+
+	public static void main(String[] args) {
+		{
+	        int arr[] = {1,2,8,9,12,46,76,82,15,20,30};
+	        int count=0;
+	        
+	        HashMap<Integer, Integer>map2 = new HashMap<Integer, Integer>();
+	          
+	        for(int i=1; i<=9; i++) 
+	        {
+	        	count=0;
+	        	for(int j=0; j<arr.length; j++)
+	        	{
+	        		if(arr[j]%i==0) 
+	        		{
+	        			count++;
+	        		}
+	        	}
+	        	map2.put(i, count); 
+	        }
+	   
+	        for (Entry<Integer, Integer> entry : map2.entrySet())
+	            System.out.print("["+ entry.getKey() +" : "+ entry.getValue()+"]");
+	    }
+	}
+
+
+	}
+
+
